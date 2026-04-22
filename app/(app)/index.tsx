f17034c1aaa05c4c7d706dd623b69f91a405bfe1
Demeris <paulodemeris725@gmail.com>
2026-04-22 12:12:27 -0300

    UIKitUpdate

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,9 +1,20 @@
 import { View, Text } from 'react-native';
+import { Button } from '@/components/ui/button'; // Nosso novo componente
+import { theme } from '@/config/themes';
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#0B192C' }}>
-      <Text style={{ color: '#FFFFFF', fontSize: 24 }}>Ambiente Configurado!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: theme.colors.background.primary, padding: 24, gap: 16 }}>
+      
+      <Text style={{ color: theme.colors.text.primary, fontSize: 24, marginBottom: 20 }}>
+        Testando o UI Kit
+      </Text>
+
+      {/* Testando as 3 variantes que identificamos no seu Figma */}
+      <Button title="Entrar na Conta" variant="primary" onPress={() => console.log('Clicou Primary!')} />
+      <Button title="Criar nova conta" variant="outline" onPress={() => console.log('Clicou Outline!')} />
+      <Button title="Excluir Consumo" variant="danger" onPress={() => console.log('Clicou Danger!')} />
+      
     </View>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,1 +1,58 @@
 import React from 'react'
+import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps, ActivityIndicator } from 'react-native'
+import { theme } from '@/config/themes'
+
+interface ButtonProps extends TouchableOpacityProps {
+    title: string;
+    variant?: 'primary' | 'danger' | 'outline';
+    isLoading?: boolean;
+}
+
+export function Button({
+    title,
+    variant = 'primary',
+    isLoading = false,
+    style,
+    ...rest
+}: ButtonProps) {
+    
+    const isOutLine = variant === 'outline';
+    const backgroundColor = isOutLine ? '#FFFFFF' : theme.colors[variant === 'primary' ? 'primary' : 'danger'].main;
+    const textColor = isOutLine ? theme.colors.text.secondary : theme.colors.text.primary
+
+    return (
+        <TouchableOpacity
+            style={[
+                styles.container,
+                { backgroundColor, borderColor: isOutLine ? theme.colors.primary.main : 'transparent' },
+                style,
+            ]}
+            activeOpacity={0.8} // Suavidade ao clicar
+            disabled={isLoading || rest.disabled}
+            {...rest}
+            >
+            {isLoading ? (
+                <ActivityIndicator color={textColor} />
+            ) : (
+                <Text style={[styles.text, { color: textColor }]}>
+                {title}
+                </Text>
+            )}
+            </TouchableOpacity>
+        );
+        }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '70%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: theme.borderRadius.sm, // Arredondamento do seu Figma
+    borderWidth: 1, // Usado apenas quando for 'outline'
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
